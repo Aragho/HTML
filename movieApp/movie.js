a604a46a0a0c3021ed55335f4d3260abcfcd3c2c
Aragho <Oluwadanny19@gmail.com>
2024-11-25 13:37:22 +0100

    codee

--- movieApp/movie.js
+++ movieApp/movie.js
@@ -1,4 +1,4 @@
-const API_KEY = "api_key=717ccf6765dcaa577801cadcc372f394";
+const API_KEY = "api_key=717ccf6765dcaa577801cadcc372f394"
 const BASE_URL = "https://api.themoviedb.org/3/"
 const SEARCHMOVIEURL = `${BASE_URL}search/movie?${API_KEY}`
 const API_URL = `${BASE_URL}movie/popular?${API_KEY}`
